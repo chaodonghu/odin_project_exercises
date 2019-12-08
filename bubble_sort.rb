367984fdb99267f3fd79c770c2046841837d3e7e
chaodonghu <cdhu@uwaterloo.ca>
2019-12-08 17:27:10 -0500

    add bubble_sort_by solution, that takes in a block

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -6,10 +6,10 @@ def bubble_sort(array)
   loop do
     swapped = false
 
-    (n-1).times do |i|
+    (n - 1).times do |i|
       if array[i] > array[i+1]
         # swap the elements in place
-        array[i], array[i+1] = array[i+1], array[i]
+        array[i], array[i + 1] = array[i + 1], array[i]
         swapped = true
       end
     end
@@ -19,3 +19,31 @@ def bubble_sort(array)
 
   array
 end
+
+bubble_sort_result = bubble_sort([4,3,78,2,0,2])
+
+puts bubble_sort_result
+
+def bubble_sort_by(array)
+  n = array.length
+
+  loop do
+    sorted = false
+    (n - 1).times do |i|
+      if yield(array[i], array[i + 1]) > 0
+         array[i], array[i + 1] = array[i+ 1], array[i]
+         sorted = true
+      end
+    end
+
+    break if not sorted
+  end
+
+  array
+end
+
+bubble_sort_by_result = bubble_sort_by(["hi","hello","hey"]) do |left,right|
+  left.length - right.length
+end
+
+puts bubble_sort_by_result
